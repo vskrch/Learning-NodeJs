cceb4fcd3ba8b3ea6f4c45d892f4c0c3478272cd
Venkata Sai Krishna Reddy Chirasani <vskrch@gmail.com>
2020-02-25 20:56:26 +0000

    event driven execution

--- index.js
+++ index.js
@@ -20,18 +20,17 @@ const server = http.createServer((req,res)=>{
             console.log(chunk);
             body.push(chunk);
         });
+
         req.on('end',() => {
                 const parsedBody = Buffer.concat(body).toString();
                 console.log(parsedBody);
                 const message = parsedBody.split('=')[1];
-                fs.writeFileSync('message.txt',message);
+                fs.writeFileSync('message.txt',message); res.statusCode=302;
+                res.setHeader('Location','/');
+                 return res.end();
         });
-
-       
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
     }
+    // registered but not executed until request methods are done 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title><head>');
