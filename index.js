603ca8218a8dd679e11ce2bd617646c2c3cd69a6
Venkata Sai Krishna Reddy Chirasani <vskrch@gmail.com>
2020-02-25 19:59:49 +0000

    changes

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const http = require('http');
+const fs= require('fs');
 const server = http.createServer((req,res)=>{
 
-const url = req.url;
-if(url == '/'){
-  res.write('<html>');
-    res.write('<head> <title> Node app</title></head>');
-      res.write('<body> ');
-        res.write('<h1> New app</h1>');
-res.setHeader('Content-type','text/html');
-console.log(res);
-
- res.end();
-
-}
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-type','text/html');
+    const url = req.url;
+    const method = req.method;
+    if(url==='/'){
+    res.write('<html>');
+    res.write('<head><title> Node App</title></head>');
+        res.write('<body> <form action ="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
 
 
- 
+    }
+    if(url === '/message' && method === 'POST'){
+
+        fs.writeFile('message.txt','usermessage');
+        res.statusCode('302');
+        res.setHeader('Location','/');
+        return res.end();
+    }
+  
+
+
 });
-
-// server.listen(3030); // obsolete in repl 
+server.listen(3030);
